6e2a97b83a34e9fab4fcb400b498b3e236b2b1ce
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2025-09-26 07:47:33 -0700

    Update store.js

--- advance/react/src/redux/store.js
+++ advance/react/src/redux/store.js
@@ -13,6 +13,6 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export default () => {
   let store = createStore(persistedReducer, applyMiddleware(thunk));
-
+  let persistor = persistStore(store);
   return { store, persistor };
 };
