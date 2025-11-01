f755fccf1207ba0ccfd88e5f2e6e9f43681888a3
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2025-11-01 14:12:05 -0700

    Update uuid.js

--- basic/day4_js/util/uuid.js
+++ basic/day4_js/util/uuid.js
@@ -2,7 +2,6 @@ function uuidv4() {
   return "10000".replace(/[018]/g, (c) =>
     (
       +c ^
-      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))
-    ).toString(16)
+
   );
 }
