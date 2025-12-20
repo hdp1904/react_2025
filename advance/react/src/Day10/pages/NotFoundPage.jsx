9a5b66a5c555531d8b02ca803342a15711140f97
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2025-12-20 13:46:38 -0800

    Update NotFoundPage.jsx

--- advance/react/src/Day10/pages/NotFoundPage.jsx
+++ advance/react/src/Day10/pages/NotFoundPage.jsx
@@ -1,16 +1,1 @@
-import React, { useEffect } from "react";
-import { useHistory } from "react-router-dom";
 
-export default function NotFoundPage() {
-  const history = useHistory();
-  useEffect(() => {
-    const id = setTimeout(() => {
-      history.push("/about");
-    }, 2000);
-
-    return clearTimeout(id);
-  }, []);
-
-  // return <Redirect to={"/"} />;
-  return <h1>404 NotFoundPage</h1>;
-}
