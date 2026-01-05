d6d25c98563f946a1d4e84662c6169d0092618c4
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2026-01-05 08:28:27 -0800

    Update index.jsx

--- advance/react/src/Day8/index.jsx
+++ advance/react/src/Day8/index.jsx
@@ -1,1 +1,19 @@
+import React from "react";
+import PropDrilling from "./PropDrilling";
+import FixPropDrilling from "./FixPropDrilling";
+import TodosList from "./TodosList";
+import { TodosProvider } from "./context/TodosContext";
+
+export default function Day8() {
+  return (
+    <div>
+      <h1>Day 8</h1>
+      {/* <PropDrilling /> */}
+      <FixPropDrilling />
+      <TodosProvider>
+        <TodosList />
+      </TodosProvider>
+    </div>
+  );
+}
 
