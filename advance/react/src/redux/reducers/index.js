ff5c3183afb7f040bb2561e85f41d95f023d72dd
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2026-01-22 09:51:16 -0800

    Combine reducers into rootReducer

--- advance/react/src/redux/reducers/index.js
+++ advance/react/src/redux/reducers/index.js
@@ -1,1 +1,14 @@
+import { combineReducers } from "redux";
+import countReducer from "./countReducer";
+import themeReducer from "./themeReducer";
+import userReducer from "./userReducer";
+
+const rootReducer = combineReducers({
+  count: countReducer,
+  theme: themeReducer,
+  user: userReducer,
+});
+
+export default rootReducer;
+
 
