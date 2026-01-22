49df15ad8a7a62a5ffd27eefcbc14906aa6780f8
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2026-01-22 09:51:25 -0800

    Remove theme reducer and toggleTheme function

--- advance/react/src/redux/reducers/themeReducer.js
+++ advance/react/src/redux/reducers/themeReducer.js
@@ -1,15 +1,1 @@
-const themeReducer = (state = "dark", action) => {
-  switch (action.type) {
-    case "THEME_TOGGLE":
-      return state === "dark" ? "light" : "dark";
 
-    default:
-      return state;
-  }
-};
-
-export function toggleTheme() {
-  return { type: "THEME_TOGGLE" };
-}
-
-export default themeReducer;
