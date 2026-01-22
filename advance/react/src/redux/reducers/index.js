13271e964a81cff332ef5fe2be9f6f3e428e299c
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2026-01-22 09:51:09 -0800

    Update index.js

--- advance/react/src/redux/reducers/index.js
+++ advance/react/src/redux/reducers/index.js
@@ -1,13 +1,1 @@
-import { combineReducers } from "redux";
-import countReducer from "./countReducer";
-import themeReducer from "./themeReducer";
-import userReducer from "./userReducer";
-
-const rootReducer = combineReducers({
-  count: countReducer,
-  theme: themeReducer,
-  user: userReducer,
-});
-
-export default rootReducer;
 
