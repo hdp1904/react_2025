cb592f7cdcb61fbd201dc6948d035b270a0e08da
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2026-04-21 09:54:47 -0700

    Update jsonServerPlay.js

--- basic/day7_async/jsonServerPlay.js
+++ basic/day7_async/jsonServerPlay.js
@@ -1,1 +1,20 @@
+// GET
+
+fetch("http://localhost:3000/posts")
+  .then((res) => res.json())
+  .then((posts) => {
+    // console.log(posts);
+  });
+
+document.querySelector("#newTodo").addEventListener("click", () => {
+  fetch("http://localhost:3000/todos", {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      task: "play",
+    }), // body data type must match "Content-Type" header
+  });
+});
 
