9018113ebc8ef58da2d3fec7d3cf6e6723935b5e
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2026-05-11 09:14:37 -0700

    Update index.jsx

--- advance/react/src/Day3/index.jsx
+++ advance/react/src/Day3/index.jsx
@@ -1,13 +1,1 @@
-import React from "react";
-import Counter from "./Counter";
-import PeopleList from "./PeopleList";
 
-export default function Day3() {
-  return (
-    <div>
-      <h1>Day3</h1>
-      {/* <Counter /> */}
-      <PeopleList />
-    </div>
-  );
-}
